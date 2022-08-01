1aa85dcb17cc20550a52833830b106c1615ce6f3
gaudes <ralf@gaudes.net>
2022-08-01 20:44:29 +0200

    Upgrade testing

--- test/integration.js
+++ test/integration.js
@@ -6,28 +6,28 @@ const { expect } = require("chai");
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.integration(path.join(__dirname, ".."),{
-	defineAdditionalTests(getHarness){
-		function sendToAsync(harness, adapter, command, options) {
-			return new Promise((resolve) => {
-				harness.sendTo(adapter, command, options, (result) => {
-					resolve(result);
-				});
+	defineAdditionalTests({suite}){
+		suite("Test sendTo()", (getHarness) => {
+			// For convenience, get the current suite's harness before all tests
+			let harness;
+			before(() => {
+				harness = getHarness();
 			});
-		}
 
-		describe("Test Message", () =>{
-			it("Calling", async () =>{
-				const harness = getHarness();
-				await harness.startAdapterAndWait();
-				await new Promise(resolve => setTimeout(resolve, 5000));
-				const result = await sendToAsync(
-					harness,
-					"fahrplan.0",
-					"getStations",
-					{ provider: "DB", station: "8000105",});
-				console.dir(result);
-				expect(result).to.be.an("array");
-				expect(result[0]).to.deep.include({ id: "8000105" });
+			it("Calling", () =>{
+				return new Promise(async (resolve) =>{
+					await harness.startAdapterAndWait();
+					harness.sendTo(
+						harness,
+						"fahrplan.0",
+						"getStations",
+						{ provider: "DB", station: "8000105"}, (result) =>{
+							console.dir(result);
+							expect(result).to.be.an("array");
+							expect(result[0]).to.deep.include({ id: "8000105" });
+							resolve();
+						} );
+				});
 			} ).timeout(12000);
 		} );
 	}} );
